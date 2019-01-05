946f25350f4b3e244ec45d78e25cd2635d0c5d01
abhijitspeed <abhijit.speed@gmail.com>
2019-01-06 01:59:21 +0530

    Test code of california earth quack.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'California Earth Quack';
+  constructor(private http: Http) { }
+  httpdata;
+  serviceData;
+  filterValue;
+   ngOnInit() {
+      this.http.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").
+      pipe(map((response) => response.json())).
+      subscribe((data) => this.displaydata(data))
+   }
+   displaydata(data) {   
+       this.httpdata = data.features;
+       let other = [];
+       this.httpdata.map(currentValue => { 
+           if(currentValue.properties.place.includes("California")){ 	
+               //console.log(currentValue.properties.place);
+               //console.log(currentValue.properties.title.substring(1, 6));
+               return {
+                   "place" : currentValue.properties.place,
+                   "scale" : currentValue.properties.title.substring(1, 6).trim()
+               };
+           } return;
+       }).forEach(item => {
+           if(item != null) {
+       		   other.push(item)
+           }
+       }
+       );
+       this.serviceData = other;
+   }
+   highLight(event: any) {
+   this.filterValue = event.target.value;
+   console.log(this.filterValue);
+   }
+}
+export class CaliforniaEarthQuack{
+    place: String;
+    scale: String;
 }
